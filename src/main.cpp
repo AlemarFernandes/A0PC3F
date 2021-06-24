760c47e04e87ed5b5af551c6c4273cc5bda5da16
Alemar Fernandes F Jr <44736112+AlemarFernandes@users.noreply.github.com>
2021-06-24 00:09:56 -0300

    💡 📝 Criada a funçao Stast

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*  ---->> A0PC3F <<----
  Projeto   RC_MASTER_TX v0.0
  
-
+ -> Criada a funçao Stast 
 
 
   By A.Fernandes
@@ -9,133 +9,142 @@
 */
 
 
+//===>> Bibliotecas Auxiliares <<===//
+
+
 #include <Arduino.h>
-//===>> Mapeamento de Hardware <<===//
-
-#define LG_1 31
-#define LR_1 30
-
-#define LG_2 29
-#define LR_2 28
-
-#define LG_3 27
-#define LR_3 26
-
-#define LG_4 25
-#define LR_4 24
 
 //------------------------------------------------------------------------------------------------------
 
-#define Tenp_1 1000
-#define Tenp_2 500
-#define Tenp_3 250
+
+//===>> Mapeamento de Hardware <<===//
+
+// Ld_A
+#define LG_1 31
+#define LR_1 30
+// Ld_B
+#define LG_2 29
+#define LR_2 28
+// Ld_C
+#define LG_3 27
+#define LR_3 26
+// Ld_D
+#define LG_4 25
+#define LR_4 24
 
 
-void Led_RG_4();
-void Led_RG_3();
-void Led_RG_2();
-void Led_RG_1();
+//------------------------------------------------------------------------------------------------------
+
+
+//===>> Constantes do Projeto <<===//
+
+#define tmp1 50
+#define tmp2 500
+#define tmp3 1000
+
+
+//------------------------------------------------------------------------------------------------------
+
+//===>> Protótipo das Funções<<===//
+
+void LD_Start (bool R1, bool G1, bool R2, bool G2, bool R3, bool G3, bool R4, bool G4);
+void LD_Start ();
+
+//------------------------------------------------------------------------------------------------------
+
+
+
 
 
 void setup()
 {
   Serial.begin(9600);
-  
+
   pinMode(LG_1, OUTPUT);
   pinMode(LR_1, OUTPUT);
-  
   pinMode(LG_2, OUTPUT);
   pinMode(LR_2, OUTPUT);
-  
   pinMode(LG_3, OUTPUT);
   pinMode(LR_3, OUTPUT);
-
   pinMode(LG_4, OUTPUT);
   pinMode(LR_4, OUTPUT);
 
+
   Serial.println("starting");
-  
 
 
 }
 
 void loop()
 {
- 
-Led_RG_4();
-Led_RG_3();
-Led_RG_2();
-Led_RG_1();
-
-
+  LD_Start();
+  
 }
 
+//===>> Desenvolvimento das Funções <<===//
 
-void Led_RG_4()
+
+
+void LD_Start ()
 {
-  Serial.println(".>.<.");
-  Serial.println("Led_G4");
-  digitalWrite(LG_4, HIGH);
-  delay(Tenp_3);
-  digitalWrite(LG_4, LOW);
-  delay(Tenp_3);
+  // LD_Start amarelos
+  LD_Start(0,0,0,0,0,0,0,0);
+  delay(tmp3);
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
+  // loop direita
+  LD_Start(0,1,1,1,0,1,1,1);
+  delay(tmp1);
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
+  LD_Start(1,0,1,1,1,0,1,1);
+  delay(tmp1);
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
+  LD_Start(1,1,0,1,1,1,0,1);
+  delay(tmp1);
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
+  LD_Start(1,1,1,0,1,1,1,0);
+  delay(tmp1);
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
 
-  Serial.println("Led_R4");
-  digitalWrite(LR_4, HIGH);
-  delay(Tenp_3);
-  digitalWrite(LR_4, LOW);
-  delay(Tenp_3);
+  
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
+  LD_Start(1,1,0,1,1,1,0,1);
+  delay(tmp1);
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
+  LD_Start(1,0,1,1,1,0,1,1);
+  delay(tmp1);
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
+  LD_Start(0,1,1,1,0,1,1,1);
+  delay(tmp1);
+  LD_Start(1,1,1,1,1,1,1,1);
+  delay(tmp1);
+
 
 }
 
 
-void Led_RG_3()
+
+
+
+void LD_Start (bool R1,  bool R2, bool R3, bool R4, bool G1, bool G2,  bool G3,  bool G4)
 {
-  Serial.println(".>.<.");
-  Serial.println("Led_G3");
-  digitalWrite(LG_3, HIGH);
-  delay(Tenp_3);
-  digitalWrite(LG_3, LOW);
-  delay(Tenp_3);
-
-  Serial.println("Led_R3");
-  digitalWrite(LR_3, HIGH);
-  delay(Tenp_3);
-  digitalWrite(LR_3, LOW);
-  delay(Tenp_3);
-
-}
-void Led_RG_2()
-{
-  Serial.println(".>.<.");
-  Serial.println("Led_G2");
-  digitalWrite(LG_2, HIGH);
-  delay(Tenp_3);
-  digitalWrite(LG_2, LOW);
-  delay(Tenp_3);
-
-  Serial.println("Led_R2");
-  digitalWrite(LR_2, HIGH);
-  delay(Tenp_3);
-  digitalWrite(LR_2, LOW);
-  delay(Tenp_3);
-
+  digitalWrite(LR_1, R1);
+  digitalWrite(LR_2, R2);
+  digitalWrite(LR_3, R3);
+  digitalWrite(LR_4, R4);
+  digitalWrite(LG_1, G1);
+  digitalWrite(LG_2, G2);
+  digitalWrite(LG_3, G3);
+  digitalWrite(LG_4, G4);
 }
 
+//------------------------------------------------------------------------------------------------------
 
-void Led_RG_1()
-{
-  Serial.println(".>.<.");
-  Serial.println("Led_G1");
-  digitalWrite(LG_1, HIGH);
-  delay(Tenp_3);
-  digitalWrite(LG_1, LOW);
-  delay(Tenp_3);
 
-  Serial.println("Led_R1");
-  digitalWrite(LR_1, HIGH);
-  delay(Tenp_3);
-  digitalWrite(LR_1, LOW);
-  delay(Tenp_3);
-
-}
